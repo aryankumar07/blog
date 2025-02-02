c94688e1534a49e3874ce4c5d49bd9b307c8e475
aryankumar07 <ak2839426@gmail.com>
2025-02-02 21:29:08 +0530

    feat(UI): skelton UI completed

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -4,6 +4,10 @@ const PostList = () => {
   return (
     <div className="flex flex-col gap-12 mb=-8">
       <PostListItem />
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
+      <PostListItem />
     </div>
   )
 }
